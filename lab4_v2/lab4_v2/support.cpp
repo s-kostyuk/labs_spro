56fb409860fdc0e6c1ef1704e92013cbea276650
Sergey Kostyuk <ks.hot.ua@gmail.com>
2015-12-07 12:25:07 +0200

    Added draft

---
+++ lab4_v2/lab4_v2/support.cpp
@@ -0,0 +1,22 @@
+#include "support.hpp"
+
+int IntRound( double _value ) {
+	// Если число больше нуля - добавляем к нему 0.5, 
+	// иначе - отнимаем 0.5
+	_value += ( _value >= 0 ) ? 0.5 : -0.5;
+
+	return (int)( _value );
+}
+
+POINT GetCenternedPosition( const SIZE & _object, const RECT & _field )
+{
+	const SIZE fieldSize{
+		_field.right - _field.left,
+		_field.bottom - _field.top
+	};
+
+	return POINT{
+		_field.left + ( ( fieldSize.cx - _object.cx ) >> 1 ) ,
+		_field.top + ( ( fieldSize.cy - _object.cy ) >> 1 )
+	};
+}
---
+++ lab4_v2/lab4_v2/support.hpp
@@ -0,0 +1,14 @@
+#ifndef _SUPPORT_HPP_
+#define _SUPPORT_HPP_
+
+#include <windows.h> // подключение библиотеки с функциями API
+
+// Предварительное описание функций
+ATOM MyRegisterClass( HINSTANCE hInstance );
+BOOL InitInstance( HINSTANCE, int );
+LRESULT CALLBACK WndProc( HWND, UINT, WPARAM, LPARAM );
+
+int IntRound( double _value );
+POINT GetCenternedPosition( const SIZE & _object, const RECT & _field );
+
+#endif //_SUPPORT_HPP_
